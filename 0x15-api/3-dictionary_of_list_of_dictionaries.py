be060e582578b24082c78f773114fd26c5304624
khalilholberton <1071@holbertonschool.com>
2020-05-04 22:22:25 +0000

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
- Python script to export data in the JSON format.
+Python script to export data in the JSON format.
 """
 
 if __name__ == "__main__":
-
     import requests
     import json
 
