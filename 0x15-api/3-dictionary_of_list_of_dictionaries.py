334c143abab2665e2adaf9648e76b6801c592c01
khalilholberton <1071@holbertonschool.com>
2020-05-04 21:52:07 +0000

    fix

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+ Python script to export data in the JSON format.
+"""
+
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    user_dict = {}
+    uname_dict = {}
+    for user in users:
+        user_id = user.get('id')
+        user_dict[user_id] = []
+        uname_dict[user_id] = user.get('username')
+
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    for task in todo:
+        task_dict = {}
+        userid = task.get('userId')
+        task_dict['username'] = uname_dict.get(userid)
+        task_dict['task'] = task.get('title')
+        task_dict['completed'] = task.get('completed')
+        user_dict.get(userid).append(task_dict)
+
+    with open('todo_all_employees.json', 'w') as jsfile:
+        json.dump(user_dict, jsfile)
