f9f5af27bac6b1d5401a4dd72b7a09650e98428e
khalilholberton <1071@holbertonschool.com>
2020-05-04 22:33:20 +0000

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,29 +2,25 @@
 """
 Python script to export data in the JSON format.
 """
-
 if __name__ == "__main__":
-    import requests
     import json
+    import requests
 
-    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    todo = requests.get("http://jsonplaceholder.typicode.com/todos").json()
-    finaljs = {}
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
+    user_dict = {}
+    uname_dict = {}
     for user in users:
-        id = user.get("id")
-        username = user.get("username")
-        list = []
-
-        for task in todo:
-            if (task.get("userId") == id and task.get("completed")):
-                d = {}
-                d["task"] = task.get("title")
-                d["completed"] = task.get("completed")
-                d["username"] = username
-                list.append(d)
-
-        finaljs[id] = list
-
-    with open("todo_all_employees.json", 'w+') as jsfile:
-        json.dump(finaljs, jsfile)
+        u_id = user.get("id")
+        user_dict[u_id] = []
+        uname_dict[u_id] = user.get('username')
+    for task in todo:
+        task_dict = {}
+        u_id = task.get("userId")
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict['username'] = uname_dict.get(u_id)
+        user_dict.get(u_id).append(task_dict)
+    with open("todo_all_employees.json", 'w') as jsfile:
+        json.dump(user_dict, jsfile)
