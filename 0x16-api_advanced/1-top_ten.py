56f367a22aa8065d8c3800a349efdae65d1c0db0
khalilholberton <1071@holbertonschool.com>
2020-05-07 20:45:51 +0000

    fix

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contain the function top_ten
+"""
+
+import requests as myreq
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    custom_user = {'User-agent': 'Khalil'}
+
+    URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+
+    query = myreq.get(URL, headers=custom_user)
+
+    if query.status_code != 200:
+        print(None)
+
+    response = query.json()
+    v = response.get('data').get('children')
+
+    for d in v:
+        print(d.get('data').get('title'))
