9361d08d2bdca710d7773efab748166deaa859dd
khalilholberton <1071@holbertonschool.com>
2020-05-07 02:01:06 +0000

    fix

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/binpython3
+# This module contain the function number_of_subscribers
+
+import requests as myreq
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
+    custom_user = {'User-agent': 'Khalil'}
+
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    query = myreq.get(URL, headers=custom_user, allow_redirects=False)
+
+    if query.status_code != 200:
+        return 0
+
+    response = query.json()
+
+    try:
+        return response.get('data').get('subscribers')
+    except Exception:
+        return 0
