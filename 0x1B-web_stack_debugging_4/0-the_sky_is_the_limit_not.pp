083f02197e118963532678426cff802acba2a4b8
khalilholberton <1071@holbertonschool.com>
2020-05-28 01:13:47 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# debug
- { 'reqload':
+# debug Nginx to accept a number of requets at the same time
+exec {'reqload':
 path     => ['/usr/bin', '/bin'],
-command  => "sudo sed -i 's/15/3000/g'  /etc/default/nginx | sudo service nginx restart",
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx |  sudo service nginx restart",
 }
