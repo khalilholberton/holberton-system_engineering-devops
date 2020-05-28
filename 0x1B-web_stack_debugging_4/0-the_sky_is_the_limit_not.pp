76f1e0b8cb02a597c5cce87b92dbd2acef58099c
khalilholberton <1071@holbertonschool.com>
2020-05-28 14:06:17 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# debug Nginx to accept a number of requets at the same time
-exec {'reqload':
-path     => ['/usr/bin', '/bin'],
-command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx |  sudo service nginx restart",
+# debug Nginx to accept a number of request at the same time
+exec {'req':
+path    => ['/usr/bin', '/bin'],
+command => "sudo sed -i 's/15/3000/g'  /etc/default/nginx;  sudo service nginx restart",
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # fix
 exec { 'holberton_hard':
-path     => ['/bin', '/usr/bin', '/usr/sbin'],
-command  => "sudo sed -i  's/holberton hard nofile 5/holberton hard nofile  4000/g' /etc/security/limits.conf; /sbin/sysctl -p",
+path    => ['/bin', '/usr/bin', '/usr/sbin'],
+command => "sudo sed -i  's/holberton hard nofile 5/holberton hard nofile  4000/g' /etc/security/limits.conf; /sbin/sysctl -p",
 }
 exec { 'holberton_soft':
-path     => ['/bin', '/usr/bin', '/usr/sbin'],
-command  => "sudo sed -i  's/holberton soft nofile 4/holberton soft nofile  4000/g' /etc/security/limits.conf; /sbin/sysctl -p",
+path    => ['/bin', '/usr/bin', '/usr/sbin'],
+command => "sudo sed -i  's/holberton soft nofile 4/holberton soft nofile  4000/g' /etc/security/limits.conf; /sbin/sysctl -p",
 }
