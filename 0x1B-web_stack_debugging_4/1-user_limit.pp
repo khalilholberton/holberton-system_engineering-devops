477c545ad10feeb7e714ba695405fce37d65d73b
khalilholberton <1071@holbertonschool.com>
2020-05-28 01:33:23 +0000

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # fix
-exec {'holberton_hard':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => "sudo sed -i  's/holberton hard nofile 5/holberton hard nofile  65534/g' /etc/security/limits.conf; /sbin/sysctl -p",
+exec { 'holberton_hard':
+path     => ['/bin', '/usr/bin', '/usr/sbin'],
+command  => "sudo sed -i  's/holberton hard nofile 5/holberton hard nofile  4000/g' /etc/security/limits.conf; /sbin/sysctl -p",
 }
-exec {'holberton_soft':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => "sudo sed -i  's/holberton soft nofile 4/holberton soft nofile  65534/g' /etc/security/limits.conf; /sbin/sysctl -p",
+exec { 'holberton_soft':
+path     => ['/bin', '/usr/bin', '/usr/sbin'],
+command  => "sudo sed -i  's/holberton soft nofile 4/holberton soft nofile  4000/g' /etc/security/limits.conf; /sbin/sysctl -p",
 }
